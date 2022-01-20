0aabbc3d00963ffb665817554e2ece519299f73b
ssoft <and.stepanov@gmail.com>
2022-01-21 00:53:59 +0300

    Add '_' in function is_<method>_Exists().

--- src/trait/IsMethodExists.h
+++ src/trait/IsMethodExists.h
@@ -40,6 +40,6 @@
     template < typename _Type, typename ... _Arguments > \
     static constexpr bool is_ ## method ## _method_exists = Is_ ## method ## _MethodExists< _Type, _Arguments ... >::value; \
     template < typename _Type, typename ... _Arguments > \
-    static constexpr bool is ## method ## MethodExists () { return Is_ ## method ## _MethodExists< _Type, _Arguments ... >::value; } \
+    static constexpr bool is_ ## method ## _MethodExists () { return Is_ ## method ## _MethodExists< _Type, _Arguments ... >::value; } \
 
 #endif
